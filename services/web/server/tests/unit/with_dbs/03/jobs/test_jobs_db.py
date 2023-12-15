10e3995a8bdb0fbfa7045a84761c73dc98664553
Mads Bisgaard <bisgaard@itis.swiss>
2023-12-15 22:49:49 +0100

    begin to implement test

--- services/web/server/tests/unit/with_dbs/03/jobs/test_jobs_db.py
+++ services/web/server/tests/unit/with_dbs/03/jobs/test_jobs_db.py
@@ -1,1 +1,8 @@
-# async def test_job_insert(user_project: ProjectDict, client: TestClient):
+from aiohttp.test_utils import TestClient
+from faker import Faker
+from simcore_service_webserver.projects.models import ProjectDict
+
+
+async def test_create_job(user_project: ProjectDict, client: TestClient, faker: Faker):
+    assert client.app
+    client.app.router
